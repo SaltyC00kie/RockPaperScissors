766e54c8665f06d214f6c40df8d34dcf170b6260
Alexander Riedl <65710822+SaltyC00kie@users.noreply.github.com>
2023-10-03 00:37:33 +0200

    added Game functionality, refactored output

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const scissors = "Scissors";
 const player1 = "Player1";
 const player2 = "Computer";
 
+
+game(); 
+
 function getComputerChoice()
 {
     const choices = [rock, paper, scissors];
@@ -29,35 +32,93 @@ function playGame(pPlayersChoice, pComputerChoice)
 {
     let winner;
 
-    switch(pPlayersChoice)
+    switch(pPlayersChoice.toUpperCase())
     {
-        case "Rock": 
+        case "ROCK": 
             if(pComputerChoice === paper)
-                winner = player2; 
-            else if(pComputerChoice === scissors)
-                winner = player1;
-            else if(pComputerChoice === rock)
-                winner = "Draw";
-            break;
-        case "Paper":
-            if(pComputerChoice === paper)
-                winner = "Draw";
-            else if(pComputerChoice === scissors)
+            {
                 winner = player2;
-            else if(pComputerChoice === rock)
+            }
+            else if(pComputerChoice === scissors)
+            {
                 winner = player1;
+            }
+            else if(pComputerChoice === rock)
+            {
+                winner = "Draw";
+            }
             break;
-        case "Scissors":
+        case "PAPER":
+            if(pComputerChoice === paper)
+            {
+                winner = "Draw";
+            }
+            else if(pComputerChoice === scissors)
+            {
+                winner = player2;
+            }
+            else if(pComputerChoice === rock)
+            {
+                winner = player1;
+            }
+            break;
+        case "SCISSORS":
             if (pComputerChoice === paper)
-                winner = player1; 
-            else if(pComputerChoice == scissors)    
+            {
+                winner = player1;
+            }
+            else if(pComputerChoice === scissors)
+            {
                 winner = "Draw";
+            }
             else if(pComputerChoice === rock)
+            {
                 winner = player2;
+            }
             break;
+        default: 
+            console.log("Wrong Input, pick between Rock Paper Scissors");
+            return;
     }
 
-    console.log("Winner is: " + winner + " - ")
+    let winningCondition;
+    if(winner === "Draw")
+    {
+        winningCondition = pPlayersChoice + " is even to " + pComputerChoice;
+    }
+    else if(winner == player1)
+    {
+        winningCondition = pPlayersChoice + " beats " + pComputerChoice;
+    }
+    else
+    {
+        winningCondition = pComputerChoice + " beats " + pPlayersChoice;
+    }
+    
+    console.log("Winner is: " + winner + " - " + winningCondition); 
+    return winner;
 }
 
-playGame("Rock", getComputerChoice());
+
+function game()
+{
+    let scorePlayer1 = 0;
+    let scorePlayer2 = 0;
+
+    for(let i = 0; i <= 5; i++)
+    {
+        let userInput = prompt("Let's play Rock Paper Scissors! Pick one")
+        let gameResult = playGame(userInput, getComputerChoice());
+
+        if(gameResult == player1)
+        {
+            scorePlayer1++;
+        }
+        if(gameResult == player2)
+        {
+            scorePlayer2++;
+        }
+    }
+
+    console.log("Winner is: " + ( (scorePlayer1 > scorePlayer2) ? player1 + " with a Score of: " + scorePlayer1 + ":" +scorePlayer2  : player2 + " with a Score of: " + scorePlayer2 + ":" + scorePlayer1));
+}
